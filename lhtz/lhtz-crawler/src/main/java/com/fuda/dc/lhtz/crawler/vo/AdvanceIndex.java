05c698d476280af3ccc01ab6eb7e89a52e331a72
aa <aa@aa-PC>
2015-10-23 19:19:50 +0800

    add some new indices

---
+++ lhtz/lhtz-crawler/src/main/java/com/fuda/dc/lhtz/crawler/vo/AdvanceIndex.java
@@ -0,0 +1,132 @@
+package com.fuda.dc.lhtz.crawler.vo;
+
+public class AdvanceIndex {
+	private double macdEma12;
+	private double macdEma26;
+	private double macdDea;
+	private double macdDiff;
+	private double macd;
+	private double mean5;
+	private double mean10;
+	private double mean20;
+	private double mean30;
+	private double mean60;
+	private double kdjK;
+	private double kdjD;
+	private double kdjJ;
+	private double obv;
+	private double rsi;
+	private double bollMd;
+	private double bollUp;
+	private double bollDn;
+	
+	public double getMacdEma12() {
+		return macdEma12;
+	}
+	public void setMacdEma12(double macdEma12) {
+		this.macdEma12 = macdEma12;
+	}
+	public double getMacdEma26() {
+		return macdEma26;
+	}
+	public void setMacdEma26(double macdEma26) {
+		this.macdEma26 = macdEma26;
+	}
+	public double getMacdDea() {
+		return macdDea;
+	}
+	public void setMacdDea(double macdDea) {
+		this.macdDea = macdDea;
+	}
+	public double getMacdDiff() {
+		return macdDiff;
+	}
+	public void setMacdDiff(double macdDiff) {
+		this.macdDiff = macdDiff;
+	}
+	public double getMacd() {
+		return macd;
+	}
+	public void setMacd(double macd) {
+		this.macd = macd;
+	}
+	public double getMean5() {
+		return mean5;
+	}
+	public void setMean5(double mean5) {
+		this.mean5 = mean5;
+	}
+	public double getMean10() {
+		return mean10;
+	}
+	public void setMean10(double mean10) {
+		this.mean10 = mean10;
+	}
+	public double getMean20() {
+		return mean20;
+	}
+	public void setMean20(double mean20) {
+		this.mean20 = mean20;
+	}
+	public double getMean30() {
+		return mean30;
+	}
+	public void setMean30(double mean30) {
+		this.mean30 = mean30;
+	}
+	public double getMean60() {
+		return mean60;
+	}
+	public void setMean60(double mean60) {
+		this.mean60 = mean60;
+	}
+	public double getKdjK() {
+		return kdjK;
+	}
+	public void setKdjK(double kdjK) {
+		this.kdjK = kdjK;
+	}
+	public double getKdjD() {
+		return kdjD;
+	}
+	public void setKdjD(double kdjD) {
+		this.kdjD = kdjD;
+	}
+	public double getKdjJ() {
+		return kdjJ;
+	}
+	public void setKdjJ(double kdjJ) {
+		this.kdjJ = kdjJ;
+	}
+	public double getObv() {
+		return obv;
+	}
+	public void setObv(double obv) {
+		this.obv = obv;
+	}
+	public double getRsi() {
+		return rsi;
+	}
+	public void setRsi(double rsi) {
+		this.rsi = rsi;
+	}
+	public double getBollMd() {
+		return bollMd;
+	}
+	public void setBollMd(double bollMd) {
+		this.bollMd = bollMd;
+	}
+	public double getBollUp() {
+		return bollUp;
+	}
+	public void setBollUp(double bollUp) {
+		this.bollUp = bollUp;
+	}
+	public double getBollDn() {
+		return bollDn;
+	}
+	public void setBollDn(double bollDn) {
+		this.bollDn = bollDn;
+	}
+	
+}
